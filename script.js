2e05ecfcf85b1a1f0d42078c2de9b64cc38f8211
RubenFontes <rubencfontes17@gmail.com>
2022-11-08 17:36:40 -0300

    fix issues

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ cpf.oninput = function () {
         this.value = this.value.slice(0, 11); 
     }
 }
+
+function mask(o, f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmask()", 1)
+}
+
+function execmask(){
+    v_obj.value = v_fun(v_obj.value)
+}
+
+function mtel(v){
+    v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+
+function id(el){
+	return document.getElementById(el);
+}
+
+window.onload = function(){
+	id('phone').onkeyup = function(){
+		mask(this, mtel);
+	}
+}
